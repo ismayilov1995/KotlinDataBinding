c06b3c19f67c7619e5c96d3120cf0ad62636d21f
ismayilov1995 <ismayilov1995@users.noreply.github.com>
2019-03-22 19:23:51 +0400

    Qirilma noqtesi

---
+++ app/src/main/java/com/ismayilov/ismayil/kotlindatabinding/adapters/ProductRVadaptor.kt
@@ -0,0 +1,23 @@
+package com.ismayilov.ismayil.kotlindatabinding.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ismayilov.ismayil.kotlindatabinding.databinding.SingleLineLayoutBinding
+import com.ismayilov.ismayil.kotlindatabinding.models.Product
+
+class ProductRVadaptor(val allProduct:ArrayList<Product>) : RecyclerView.Adapter<ProductRVadaptor.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding : SingleLineLayoutBinding = SingleLineLayoutBinding.inflate(LayoutInflater.from(parent.context))
+        return MyViewHolder(binding)
+    }
+    override fun getItemCount(): Int {
+        return allProduct.size
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.binding.product = allProduct[position]
+        holder.binding.executePendingBindings()
+    }
+    inner class MyViewHolder(var binding: SingleLineLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/ismayilov/ismayil/kotlindatabinding/fragments/MainFragment.kt
+++ app/src/main/java/com/ismayilov/ismayil/kotlindatabinding/fragments/MainFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.ismayilov.ismayil.kotlindatabinding.adapters.ProductRVadaptor
 import com.ismayilov.ismayil.kotlindatabinding.databinding.FragmentMainBinding
+import com.ismayilov.ismayil.kotlindatabinding.models.Product
+import com.ismayilov.ismayil.kotlindatabinding.utils.AllProduct
 
 class MainFragment : Fragment() {
 
@@ -19,6 +23,15 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        val lm = GridLayoutManager(activity,2)
+        val allProduct = AllProduct()
+        val mAdapter = ProductRVadaptor(allProduct.tumProductlerDizisi.toCollection(ArrayList<Product>()))
+        binding.productRvList.layoutManager = lm
+        binding.productRvList.adapter = mAdapter
+
+        binding.refreshLayout.setOnRefreshListener {
+            binding.refreshLayout.isRefreshing = false
+        }
 
         return binding.root
     }
